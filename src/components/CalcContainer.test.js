95e67c6938d766ad9f68c7eb2ae283f9075a46b1
mcollums <collumsmichelle@gmail.com>
2019-10-01 11:09:28 -0500

    Completed tests for #1

--- src/components/CalcContainer.test.js
+++ src/components/CalcContainer.test.js
@@ -9,7 +9,7 @@ import CalculatorContainer from './CalcContainer';
 
 //Create ShallowWrapper for the App Content
 const setup = (props={}, state=null) => {
-    const wrapper = shallow(<CalculatorContainer {...props}/>);
+    const wrapper = mount(<CalculatorContainer {...props}/>);
     if (state) wrapper.setState(state);
     return wrapper;
 }
@@ -20,11 +20,6 @@ describe('Components should render without error', () => {
         const wrapper = setup();
         const submitButton = wrapper.find("[data-test='submit-button']");
         expect(submitButton.length).toBe(1);
-    });
-    test('Input Box', () => {
-        const wrapper = setup();        
-        const inputBox = wrapper.find("[data-test='input-group']");
-        expect(inputBox.length).toBe(1);
     });
     test('Result Display', () => {
         const wrapper = setup();
@@ -53,19 +48,60 @@ describe('Starting State should be blank', () => {
 });
 
 
-//define empty mock function 
-// describe('startAdd', () => {
-//     test('clicking submit button should display results', () => {
-//         const numString = "1,2";
-//         const wrapper = setup(null, { numString });
+//FUNCTION TESTS
+describe('startAdd', () => {
+    
+    test('String "1,2" should display 3', () => {
+        const string = "1,2";
+        const wrapper = setup(null, { string });
 
-//         const submitButton = wrapper.find("[data-test='submit-button']");
-//         submitButton.simulate('click');
-//         wrapper.update();
+        const submitButton = wrapper.find("[data-test='submit-button']");
+        submitButton.simulate('click');
+        wrapper.update();
 
-//         // find display and test value
-//         const resultDisplay = wrapper.find("[data-test='result-display']");
-//         expect(resultDisplay.text()).toContain(3);
-//     });
-// });
+        // find display and test value
+        const resultDisplay = wrapper.find("[data-test='result-display']");
+        expect(resultDisplay.text()).toContain(3);
+    });
+
+    test('String "3,4" should display 7', () => {
+        const string = "3,4";
+        const wrapper = setup(null, { string });
+
+        const submitButton = wrapper.find("[data-test='submit-button']");
+        submitButton.simulate('click');
+        wrapper.update();
+
+        // find display and test value
+        const resultDisplay = wrapper.find("[data-test='result-display']");
+        expect(resultDisplay.text()).toContain(7);
+    });
+
+    test('String "cat,4" should display 4', () => {
+        const string = "cat,4";
+        const wrapper = setup(null, { string });
+
+        const submitButton = wrapper.find("[data-test='submit-button']");
+        submitButton.simulate('click');
+        wrapper.update();
+
+        // find display and test value
+        const resultDisplay = wrapper.find("[data-test='result-display']");
+        expect(resultDisplay.text()).toContain(4);
+    });
+
+    test('Three items in the string should throw an error', () => {
+        const string = "1,3,4";
+        const wrapper = setup(null, { string });
+
+        const submitButton = wrapper.find("[data-test='submit-button']");
+        submitButton.simulate('click');
+        wrapper.update();
+
+        // find error display see if the class contains hidden
+        const errorDisplay = wrapper.find("[data-test='error-display']");
+        const errorHasHiddenClass = errorDisplay.hasClass('hidden');
+        expect(errorHasHiddenClass).toBe(false);
+    });
+});
 
